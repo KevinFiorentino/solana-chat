8aac3e51aab59c270d1e09035d5c57358087d17f
Kevin <kevinfiorentino.1294@gmail.com>
2022-10-19 23:03:15 -0300

    Add timestamp to contract, unit test

--- programs/solana-chat/src/lib.rs
+++ programs/solana-chat/src/lib.rs
@@ -11,8 +11,11 @@ pub mod solana_chat {
     ) -> Result<()> {
 
         let message = &mut ctx.accounts.message;
+        let clock: Clock = Clock::get().unwrap();
+
         message.owner = ctx.accounts.user.key();
         message.text = text;
+        message.timestamp = clock.unix_timestamp;
 
         Ok(())
     }
@@ -25,15 +28,17 @@ pub struct CreateMessage<'info> {
     #[account(
         init,
         payer = user,
-        seeds = [
+        /* seeds = [
             b"create_message",
             user.key().as_ref(),
-            text.as_bytes()
+            text.as_bytes(),
+            get_date().to_string().as_bytes()    // I'm not sure how generate a random seed with the date
         ],
-        bump,
+        bump, */
         space = 8  +                             // Discriminator Anchor
                 32 +                             // Owner
-                text.as_bytes().len() + 4        // Text + 4
+                text.as_bytes().len() + 4 +      // Text + 4    (String prefix)
+                8                                // Timestamp
     )]
     pub message: Account<'info, Message>,
 
@@ -48,4 +53,5 @@ pub struct CreateMessage<'info> {
 pub struct Message {
     pub owner: Pubkey,    // 32
     pub text: String,     // n + 4
+    pub timestamp: i64,   // 8
 }
--- tests/solana-chat.ts
+++ tests/solana-chat.ts
@@ -4,61 +4,134 @@ import { SolanaChat } from '../target/types/solana_chat';
 import { TextEncoder } from 'util';
 import { expect } from "chai";
 
-const { PublicKey, SystemProgram } = anchor.web3;
-
-const stringToBytes = (input: string): Uint8Array => {
-  return new TextEncoder().encode(input);
-};
+const { PublicKey, SystemProgram, Keypair, Connection, clusterApiUrl } = anchor.web3;
 
 describe('solana-chat', () => {
 
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.SolanaChat as Program<SolanaChat>;
-  const wallet = anchor.Wallet.local();
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   it('Create message', async () => {
 
     const text = 'Test';
+    
+    const msg = anchor.web3.Keypair.generate();
 
+    /*
+    const date = new Date();
     const [pda] = await PublicKey.findProgramAddress(
       [
-        stringToBytes('create_message'),
-        wallet.publicKey.toBytes(),
-        stringToBytes(text),
+        anchor.utils.bytes.utf8.encode('create_message'),
+        provider.wallet.publicKey.toBuffer(),
+        anchor.utils.bytes.utf8.encode(text),
+        anchor.utils.bytes.utf8.encode(Math.floor(date.getTime() / 1000).toString())
       ],
       program.programId
     );
-
-    let txId = await program.methods
+    */
+    
+    const txId = await program.methods
       .createMessage(text)
       .accounts({
-        message: pda,
-        user: wallet.publicKey,
+        message: msg.publicKey,
+        user: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
       })
+      .signers([msg])
       .rpc();
 
     expect(txId).to.be.a('string');
   });
 
-  
-  it('Get all messages', async () => {
-    const messages = await program.account.message.all();
-    expect(messages.length).equal(1);
+
+  it('Create message 2', async () => {
+
+    const text = 'Test';
+    
+    const msg = anchor.web3.Keypair.generate();
+
+    const txId = await program.methods
+      .createMessage(text)
+      .accounts({
+        message: msg.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([msg])
+      .rpc();
+
+    expect(txId).to.be.a('string');
+
+    const msgAccount = await program.account.message.fetch(msg.publicKey);
+
+    // console.log(msgAccount.timestamp.toString())
+
+    expect(msgAccount.owner.toBase58()).equal(provider.wallet.publicKey.toBase58());
+    expect(msgAccount.text).equal('Test');
+  });
+
+
+  it('Create message from different user', async () => {
+
+    const text = 'Test from different user';
+
+    const otherUser = anchor.web3.Keypair.generate();
+
+    const airdropSignature = await program.provider.connection.requestAirdrop(
+      otherUser.publicKey,
+      1000000000
+    );
+    
+    const latestBlockHash = await program.provider.connection.getLatestBlockhash();
+    
+    await program.provider.connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: airdropSignature,
+    });
+
+    
+    const msg = anchor.web3.Keypair.generate();
+
+    const txId = await program.methods
+      .createMessage(text)
+      .accounts({
+        message: msg.publicKey,
+        user: otherUser.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([otherUser, msg])
+      .rpc();
+
+    expect(txId).to.be.a('string');
+
+    const msgAccount = await program.account.message.fetch(msg.publicKey);
+
+    expect(msgAccount.owner.toBase58()).equal(otherUser.publicKey);
+    expect(msgAccount.text).equal('Test from different user');
   });
 
   
-  it("Finds messages by owner", async () => {
+  it('Get all messages (3)', async () => {
+    const messages = await program.account.message.all();
+    expect(messages.length).equal(3);
+  });
+
+  
+  it("Finds messages by owner (2)", async () => {
     const msgByOwner = await program.account.message.all([
       {
         memcmp: {
-          bytes: wallet.publicKey.toBase58(),
+          bytes: provider.wallet.publicKey.toBase58(),
           offset: 8,
         },
       },
     ]);
-    expect(msgByOwner.length).equal(1);
+    expect(msgByOwner.length).equal(2);
   });
 
 });
